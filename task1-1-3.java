b5fa5abffd9338ec62de6ce2937a104ffde3c38b
reym1chael <94875041+reym1chael@users.noreply.github.com>
2025-04-29 04:45:19 +0000

    done task 1-1-3

--- task1-1-3.java
+++ task1-1-3.java
@@ -1,1 +1,26 @@
+class loopTasks {
+    public static void main(String[] args) {
+        int check_number = 10;
+        int i = 0;
+        String message;
+        System.out.println("");
+        System.out.println("");
+        System.out.println("USING WHILE LOOP:");
+        while (i <= check_number){
+            message = (i%2==0) ? i+" is even": i+" is odd";
+            i++;
+            System.out.println(message);
+        }
 
+        i = 0;
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("USING FOR LOOP:");
+
+        for (int x = 0; x <= check_number; x++){
+            message = (x%2==0) ? x+" is even": x+" is odd";
+            System.out.println(message);
+        }
+    }
+}
