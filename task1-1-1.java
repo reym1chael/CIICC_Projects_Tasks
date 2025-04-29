ca4ffee04f379f894e2c70d466bb07df93318748
reym1chael <94875041+reym1chael@users.noreply.github.com>
2025-04-29 02:48:50 +0000

    done 1st task

--- task1-1-1.java
+++ task1-1-1.java
@@ -1,1 +1,10 @@
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class helloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!");
 
+    }
+}
