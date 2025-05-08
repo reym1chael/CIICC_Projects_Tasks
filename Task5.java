9d470029e7d194a4b677554e89c0bf376161b615
reym1chael <94875041+reym1chael@users.noreply.github.com>
2025-05-08 14:06:15 +0000

    DONE TASK 5

--- Task5.java
+++ Task5.java
@@ -1,27 +1,44 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Task5 {
     public static void main(String[] args) {
-        // check kung gumana
+        //int[] numbers1 = new int[] {75,75,75};
+
         Scanner scanner = new Scanner(System.in);
-        //Integer [] numbers = {3, 2, 1};
-        int [] numbers = {3, 2, 1};
-        System.out.println(numbers[2]);
-        //Arrays.sort(numbers, Collections.reverseOrder());
-        Collections.sort(Arrays.asList(numbers), Collections.reverseOrder());
-        for (int i = 0; i < numbers.length; i++){
-            System.out.println(numbers[i] + "");
-        }
-        ArrayList<Integer> numbers3 = new ArrayList<>();
-        while (true) {
-            String input = scanner.nextLine();
-            if (input.equalsIgnoreCase("done")) {
-                break; 
+
+        int numSize = 3; // INDEX SIZE
+
+        int [] numbers1 = new int[numSize]; //ASSIGNED INDEX SIZE
+
+        System.out.println("\n\nEnter 3 NUMBERS only.");
+
+        for (int i = 0; i < numSize; i++) {
+            if (i == 0){
+                System.out.print("Enter the FIRST Number: ");
+                numbers1[i] = scanner.nextInt();
             }
-            numbers3.add(input);
+            if (i == 1){
+                System.out.print("Enter the SECOND Number: ");
+                numbers1[i] = scanner.nextInt();
+            }
+            if (i == 2){
+                System.out.print("Enter the THIRD Number: ");
+                numbers1[i] = scanner.nextInt();
+            }   
         }
-    }
+
+        Arrays.sort(numbers1); // SORTS IN ASCENDING ORDER
+
+        if ((numbers1[0] == numbers1[1]) && (numbers1[1] == numbers1[2]) ){ // CHECKS IF ALL NUMBERS HERE ARE EQUAL
+            System.out.println("All numbers are equal.");
+        }
+        else {
+            System.out.println("Largest number is: " + numbers1[2]); // GETS THE NUMBER FROM LAST INDEX FROM SORTED ARRAY
+        };
+        
+        scanner.close();
+        
+
+}
 }
