ac39c9ea7d4c41ee6458dc3e83da2e9191db137f
reym1chael <94875041+reym1chael@users.noreply.github.com>
2025-05-08 12:18:31 +0000

    trying to commit

---
+++ Task5.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //Integer [] numbers = {3, 2, 1};
+        int [] numbers = {3, 2, 1};
+        System.out.println(numbers[2]);
+        //Arrays.sort(numbers, Collections.reverseOrder());
+        Collections.sort(Arrays.asList(numbers), Collections.reverseOrder());
+        for (int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i] + "");
+        }
+        ArrayList<Integer> numbers3 = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("done")) {
+                break; 
+            }
+            numbers3.add(input);
+        }
+    }
+}
