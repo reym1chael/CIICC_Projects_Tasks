493c6b1649cf84ca62d3ba723e4b263976c80401
reym1chael <94875041+reym1chael@users.noreply.github.com>
2025-05-09 10:13:33 +0000

    DONE TASK 6

--- Task6.java
+++ Task6.java
@@ -2,7 +2,7 @@ public class Task6{
     public static void main(String[] args) {
         int[] numbers = {951, 402, 984, 651, 360, 69, 408, 319, 601, 485, 
             980, 507, 725, 547, 544, 15, 83, 165, 141, 501, 263, 617, 865, 
-            575, 219, 390, 984, 592, 236, 105, 942, 941 386, 462, 47, 418, 
+            575, 219, 390, 984, 592, 236, 105, 942, 941, 386, 462, 47, 418, 
             907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345, 399, 
             162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 
             687, 217, 815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 
@@ -13,8 +13,16 @@ public class Task6{
         // numbers list in the same order they are received. Dont print any numbers that come after 237 in the sequence.
         // START OF MY CODE
 
-        
+        System.out.println("All the even number until 237: ");
 
-
+        for (int num: numbers){
+            if (num%2 == 0) {
+                System.out.println(num);
+                }
+            if (num == 237){ // DETECTING NUMBER 237 THUS ENDING THE LOPP
+                System.out.println("The number 237 is detected. Loop breaking... ");
+                break;
+                }
+        }
     }
 }
