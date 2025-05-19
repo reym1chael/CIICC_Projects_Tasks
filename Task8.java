0f8439e9c6f11cccd40f36e1067e49c4e90a8f52
reym1chael <94875041+reym1chael@users.noreply.github.com>
2025-05-19 03:38:25 +0000

    optimized and shortened

--- Task8.java
+++ Task8.java
@@ -9,19 +9,24 @@ public class Task8 {
 
     public int sumOfAllParameters (int...x){
         
-        System.out.println("Parameters: " + x[0] + ", " + x[1] + ", " + x[2]); // how to add all indexes that depends on the input in main method without manual input
-
+        //System.out.println("Parameters: " + x[0] + ", " + x[1] + ", " + x[2]); // how to add all indexes that depends on the input in main method without manual input
+        
         int a = 0;
         int b = 0;
         int sum = 0;
+        System.out.print("\nParameters: ");
         for (a = 0; a < x.length; a++){
-            System.out.println("Values of parameters " + x[a]);
+            System.out.print("["+x[a]+"] ");
+        }
+        System.out.println("\n");
+        for (a = 0; a < x.length; a++){
+            System.out.println("Parameter: [" + x[a]+"]");
             int sum2 = 0;
-            for (b = 0; b <= x[a]; b++){
-                System.out.println("Values of B: " + b);
+            for (b = 1; b <= x[a]; b++){
+                System.out.println("Subsequent values of parameter [" + x[a] + "]: " + b);
                 sum2 += b;
             }
-            System.out.println("Sum of B: " + sum2);
+            System.out.println("Sum of subsequent values: " + sum2);
             System.out.println("\n");
             sum += sum2;
         }
@@ -31,7 +36,7 @@ public class Task8 {
     public static void main(String[] args) {
 
         Task8 objTask8 = new Task8();
-        System.out.println("Sum of all (cummulative) parameters : " + objTask8.sumOfAllParameters(4,5,10,11));
+        System.out.println("Sum of all (cummulative) parameters : " + objTask8.sumOfAllParameters(1,2,3));
 
     }
 
---
+++ Task8ver2.java
@@ -0,0 +1,39 @@
+public class Task8ver2 {
+    public int sumOfAllParameters1 (int...x){
+        System.out.println("Version 2.");
+        //System.out.println("Parameters: " + x[0] + ", " + x[1] + ", " + x[2]); // how to add all indexes that depends on the input in main method without manual input
+        
+        int a = 0;
+        int b = 0;
+        int sum = 0;
+        System.out.print("\nParameters: ");
+        for (a = 0; a < x.length; a++){
+            System.out.print("["+x[a]+"] ");
+        }
+        System.out.println("\n");
+        for (a = 0; a < x.length; a++){
+            System.out.println("Parameter: [" + x[a]+"]");
+            int sum2 = 0;
+            
+            for (b = 1; b <= x[a]; b++){
+                
+                System.out.print(b);
+                if (b != x[a]){
+                    System.out.print("+");
+                }
+                sum2 += b;
+            }
+            System.out.print("=" + sum2);
+            System.out.println("\n");
+            sum += sum2;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        Task8ver2 objTask8ver2 = new Task8ver2();
+        System.out.println("Sum of all (cummulative) parameters : " + objTask8ver2.sumOfAllParameters1(4, 5, 10));
+
+    }
+}
