88121ff0ff42e6568f44d2ee05b1bdab26fa31b3
reym1chael <94875041+reym1chael@users.noreply.github.com>
2025-05-25 04:12:23 +0000

    added task 11

--- Task11.java
+++ Task11.java
@@ -4,15 +4,13 @@ class Book{
     private String author;
     private int yearPublished;
     private double price;
-
-    // constructor
+    
     public Book (String title, String author, int yearPublished, double price) {
         this.title = title;
         this.author = author;
         this.yearPublished = yearPublished;
         this.price = price;
     }
-
 
     @Override
     public String toString() {
@@ -22,30 +20,23 @@ class Book{
                "\nYear Published: " + yearPublished +
                "\nPrice: $" + price + "\n"
         ;
-        
-
     }
-
 }
 
 // separate class (?)
 class TestClass{
-
     public static void main(String[] args) {
-        
         // instantiate, code style from Task 10
         Book [] books = new Book[]{
             new Book("Java Programming","John Smith", 2021, 39.99),
             new Book("Python Basics","Jane Doe", 2020, 29.99),
             new Book("C++ Essentials","Michael Johnsons", 2019, 49.99)
             };
-            
             for (int i = 1; i <= books.length; i++){
                 System.out.println("Book " + i);
                 System.out.println(books[i-1]);
             }
     }
-    
 }
 
 public class Task11 {
